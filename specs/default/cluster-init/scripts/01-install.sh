2a0137edc97400da465dabe4658d79686d975b8a
Aditi Gaur <aditigaur@microsoft.com>
2025-12-02 10:58:04 -0800

    Change perms and fix initialization

--- specs/default/cluster-init/scripts/01-install.sh
+++ specs/default/cluster-init/scripts/01-install.sh
@@ -5,7 +5,9 @@ set -x
 set -e
 DATADIR=$(jetpack config cyclecloud.mounts.db.mountpoint)
 # Move the MySQL data directory to the block device
-
+if [[ -d "$DATADIR/mysql" ]]; then
+	rm -rf $DATADIR/mysql
+fi
 mkdir -p $DATADIR/mysql
 # Set Correct Permissions
 chown -R mysql:mysql $DATADIR/mysql
